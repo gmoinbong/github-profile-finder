9b1da2369cdc5508e3237727633583848d740cff
gmoinbong <vladosos.scherba@gmail.com>
2023-07-03 23:26:18 +0300

    readme

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import styles from './Header.module.scss';
 export const Header = () => (
   <div className={styles.header} >
     <div className={styles.logo}>
-      devfinder
+      GHFinder
     </div>
     <ThemeSwitcher />
   </div>
